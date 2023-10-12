a7138860352505076cdff6673ed34724f638070c
Ashwanikumarshaw <Ashwanikumar11shaw@gmail.com>
2023-10-12 23:27:34 +0530

    MaximumPathSumInTheMatrix

---
+++ src/gridAnd2D/MaximumPathSumInTheMatrix.java
@@ -0,0 +1,21 @@
+package gridAnd2D;
+
+public class MaximumPathSumInTheMatrix {
+	/*
+	 * You have been given an N*M matrix filled with integer numbers, find the
+	 * maximum sum that can be obtained from a path starting from any cell in the
+	 * first row to any cell in the last row. From a cell in a row, you can move to
+	 * another cell directly below that row, or diagonally below left or right. So
+	 * from a particular cell (row, col), we can move in three directions i.e.
+	 * 
+	 * Down: (row+1,col) 
+	 * Down left diagonal: (row+1,col-1) 
+	 * Down right diagonal: (row+1, col+1)
+	 * 
+	 */
+	public static void main(String[] args) {
+		
+
+	}
+
+}
